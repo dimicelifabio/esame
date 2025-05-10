9e1a0a2dbcb56306ed8e4d22e3291e1caaad181c
dimiceliFabio <fabio25mich@gmail.com>
2025-05-10 13:18:35 +0200

    chore: initialize project

---
+++ src/main/java/com/exam/esame/EsameApplication.java
@@ -0,0 +1,13 @@
+package com.exam.esame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/esame/EsameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.esame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
