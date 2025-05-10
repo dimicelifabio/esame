fcbe4c6569e33c7b7014d4f70ab5c43530253f79
dimiceliFabio <fabio25mich@gmail.com>
2025-05-10 15:12:17 +0200

    chore: create corso.java provaEsame.java e studente.java with tables on database

---
+++ src/main/java/com/exam/esame/model/ProvaEsame.java
@@ -0,0 +1,36 @@
+package com.exam.esame.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "prove_esami")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProvaEsame {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "studente_id", nullable = false)
+    private Studente studente;
+
+    @ManyToOne
+    @JoinColumn(name = "corso_id", nullable = false)
+    private Corso corso;
+
+    @Column(nullable = false)
+    private Integer voto;
+}
